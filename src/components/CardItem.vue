16d512a2876dcca432268b9d247d69bf9cece102
mingmoth <mingmoth@gmail.com>
2023-08-07 16:21:06 +0800

    feat: add CardItem component & css

---
+++ src/components/CardItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    card: {
+        type: Object
+    }
+});
+
+const card = computed(() => props.card);
+</script>
+
+<template>
+    <figure class="card">
+        <img
+            :src="card.download_url"
+            alt="card-url"
+            class="card-image"
+        >
+        <figcaption class="card-info">
+            <div><b>{{ card.author }}</b></div>
+            <span>width: {{ card.width }}</span>
+            <span>height: {{ card.height }}</span>
+        </figcaption>
+        <div class="card-index">{{ Number(card.id) + 1 }}</div>
+    </figure>
+</template>
+
+<style lang="scss" scoped>
+@import '../style/carditem.scss';
+
+</style>
