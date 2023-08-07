69fc16f6d21e8c78af5e79a2e27677dbeb8f058c
mingmoth <mingmoth@gmail.com>
2023-08-07 14:01:05 +0800

    chore: add eslint standard for vue in vite

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
