aa8b80a092df16cdea652ac98c3dcb77d07822e7
mingmoth <mingmoth@gmail.com>
2023-08-07 17:05:40 +0800

    feat: add LoadingCard component

---
+++ src/components/LoadingCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Loading from './LoadingShimmer.vue';
+</script>
+
+<template>
+    <figure class="card">
+        <div class="card-image">
+            <Loading />
+        </div>
+        <figcaption class="card-info">
+            <Loading class="card-loading-block"/>
+            <Loading class="card-loading-block"/>
+            <Loading class="card-loading-block"/>
+        </figcaption>
+    </figure>
+</template>
+
+<style lang="scss" scoped>
+@import '../style/carditem.scss';
+.card {
+    height: 200px;
+
+    &-loading-block {
+        height: 20px;
+    }
+}
+
+</style>
