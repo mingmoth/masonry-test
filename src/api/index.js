9a8d9a765e26e838576549d3b1b64e7b30dbecea
mingmoth <mingmoth@gmail.com>
2023-08-07 16:19:34 +0800

    feat: add fetch api index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,19 @@ import axios from 'axios';
 export const imgsPerFetch = 10;
 export const fetchUrl = 'https://picsum.photos/v2/list';
 
-export default async function getImages (page = 1) {
-    const response = await axios.get(`${fetchUrl}?page=${page}&limit=${imgsPerFetch}`);
-    console.log('repsonse', response);
+export default async function getImages (page = 3) {
+    try {
+        const response = await axios.get(`${fetchUrl}?page=${page}&limit=${imgsPerFetch}`);
+        if (response.status !== 200 || !Array.isArray(response.data)) {
+            throw new Error('Can not fetch images');
+        }
+        await sleep(2000);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function sleep (delay) {
+    return new Promise(resolve => setTimeout(resolve, delay));
 }
