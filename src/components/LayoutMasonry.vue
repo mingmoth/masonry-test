72ea551794eabf9338f1ec3a323bfebf12ce8440
mingmoth <mingmoth@gmail.com>
2023-08-07 17:07:11 +0800

    feat: adjust layout css

--- src/components/LayoutMasonry.vue
+++ src/components/LayoutMasonry.vue
@@ -9,9 +9,9 @@
 
 <style lang="scss" scoped>
 .masonry {
-    // column-count: 5;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
-    gap: 1rem;
+    column-count: 5;
+    // display: grid;
+    // grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+    // gap: 1rem;
 }
 </style>
