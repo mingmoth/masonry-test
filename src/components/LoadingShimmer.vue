4e472f7e5fb01d5e4f939022729db3abc63fbb6b
mingmoth <mingmoth@gmail.com>
2023-08-07 17:03:12 +0800

    feat: add LoadingShimmer component

---
+++ src/components/LoadingShimmer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="loading"></div>
+</template>
+
+<style lang="scss" scoped>
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
+}
+
+.loading {
+    position: relative;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    background: #eee;
+    background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+    border-radius: 8px;
+    background-size: 200% 100%;
+    animation: 1.5s shine linear infinite;
+}
+</style>
