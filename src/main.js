3d032c4bfc36c57d0727040eb34f1160e29e1510
mingmoth <mingmoth@gmail.com>
2023-08-08 13:11:11 +0800

    feat: import VueMasonryPlugin from vue-masonry into main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue';
 
 // packages
 import MasonryWall from '@yeger/vue-masonry-wall';
+import { VueMasonryPlugin } from 'vue-masonry';
 
 const app = createApp(App);
 
 app.use(MasonryWall);
+app.use(VueMasonryPlugin);
 
 app.mount('#app');
